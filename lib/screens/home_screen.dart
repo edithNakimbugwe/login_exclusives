8653e82092b3fe85922eaa8fba452ddc3b8319ab
Edith Nakimbugwe <shanitadith@gmail.com>
2024-06-18 11:27:58 +0300

    changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          'Home Screen',
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Times New Roman',
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
+        )),
+        backgroundColor: Colors.orange,
+        elevation: 0,
+      ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            Text('This wll beb the home page'),
-            Text('the buttons will be put here')
+            Text(
+              'This wll beb the home page',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Times New Roman',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'the buttons will be put here',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Times New Roman',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            )
           ],
         ),
       ),
