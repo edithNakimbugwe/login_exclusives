9b2b2c0568dc78bc335f46586fba513c814aecbd
Edith Nakimbugwe <shanitadith@gmail.com>
2024-06-20 12:29:38 +0300

    MansonryGridView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,29 +20,52 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.orange,
         elevation: 0,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'This wll beb the home page',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Times New Roman',
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold),
+          children: [
+            Flexible(
+              child: MasonryGridView.count(
+                crossAxisCount: 2,
+                itemCount: 1000,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 160,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Text("Item $index"),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {}, child: const Text("Buy")),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            ElevatedButton(
+                                onPressed: () {}, child: const Text("Cart")),
+                          ],
+                        )
+                      ],
+                    )),
+                  );
+                },
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'the buttons will be put here',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Times New Roman',
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold),
-            )
           ],
         ),
       ),
