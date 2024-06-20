ca3869b7acbcb4ecc8cec97b1d170fd76629f7e8
Edith Nakimbugwe <shanitadith@gmail.com>
2024-06-20 14:20:32 +0300

    MansonryGridView modifications

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,100 +24,75 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Flexible(
-          child: Column(
-            children: [
-              Container(
-                width: 500,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Welcome onboard ...!",
-                    style: TextStyle(
+        child: Column(
+          children: [
+            Flexible(
+              child: MasonryGridView.count(
+                crossAxisCount: 2,
+                itemCount: 1000,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 160,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.orange[100],
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
                         color: Colors.black,
-                        fontFamily: 'Times New Roman',
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Flexible(
-                child: MasonryGridView.count(
-                  crossAxisCount: 2,
-                  itemCount: 1000,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 160,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        color: Colors.orange[100],
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        ),
+                        width: 2,
                       ),
-                      child: Center(
-                          child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Item $index"),
-                          const SizedBox(
-                            height: 35,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  Get.to(const LoginScreen());
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  foregroundColor: Colors.black,
-                                  backgroundColor: Colors.orange,
-                                  elevation: 10,
-                                ),
-                                child: const Text(
-                                  'Buy',
-                                  style: TextStyle(color: Colors.black),
-                                ),
+                    ),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Item $index"),
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Get.to(const LoginScreen());
+                              },
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.orange,
+                                elevation: 10,
                               ),
-                              const SizedBox(
-                                width: 5,
+                              child: const Text(
+                                'Buy',
+                                style: TextStyle(color: Colors.black),
                               ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  foregroundColor: Colors.black,
-                                  backgroundColor: Colors.orange,
-                                  elevation: 10,
-                                ),
-                                child: const Text(
-                                  'Cart',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      )),
-                    );
-                  },
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.orange,
+                                elevation: 10,
+                              ),
+                              child: const Text(
+                                'Cart',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )),
+                  );
+                },
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
